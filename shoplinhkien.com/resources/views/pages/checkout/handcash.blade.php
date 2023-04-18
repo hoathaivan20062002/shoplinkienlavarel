4769a2111a7e8acc5628a864b121581d8e5800d4
hoathaivan20062002 <thaivanhoa2002@gmail.com>
2023-04-19 00:47:16 +0700

    first commit

---
+++ shoplinhkien.com/resources/views/pages/checkout/handcash.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+@section('content')
+
+<section id="cart_items">
+		<div class="container">
+
+			<div class="review-payment">
+				<h2>Cảm ơn bạn đã đặt hàng tại  shop linh kiện,chúng tôi sẽ liên hệ với bạn sớm nhất 🥰🥰🥰</h2>
+			</div>
+
+
+
+		</div>
+	</section> <!--/#cart_items-->
+
+@endsection
